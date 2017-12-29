f139213390b8b6129a285d5e9da44fd95fb89db1
santosh kumar singh <santosh.ksingh03@gmail.com>
2017-12-29 16:42:09 +0530

    Added new project wcf json

---
+++ WCF_JSONP/WCFClient/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WCFClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/jquery-3.2.1.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#btn").click(function () {
+                $.ajax({
+                    type: "GET",
+                    dataType: "jsonp",
+                    url: "http://localhost:9090/GetAll",
+                    success: function (data) {
+                        console.log(data);
+                    }
+                });
+            })
+        })
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input type="button" id="btn" value="GetData" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WCF_JSONP/WCFHost/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFHost
+{
+
+
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember] public string Name { get; set; }
+        [DataMember] public DateTime OrderDate { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IOrder
+    {
+        [OperationContract]
+        [WebGet(UriTemplate ="/GetAll",RequestFormat =WebMessageFormat.Json,ResponseFormat =WebMessageFormat.Json)]
+        List<Order> GetAll();
+    }
+
+
+    [ServiceBehavior]
+    public class OrderService : IOrder
+    {
+        [OperationBehavior]
+        public List<Order> GetAll()
+        {
+            return new List<Order>()
+            {
+                new Order{Id=1,Name="Mobile",OrderDate=DateTime.Now.AddHours(1)},
+                new Order{Id=2,Name="Laptop",OrderDate=DateTime.Now.AddHours(2)},
+            };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(OrderService));
+            host.Open();
+            Console.WriteLine("Service is ready");
+            Console.ReadLine();
+        }
+    }
+}
